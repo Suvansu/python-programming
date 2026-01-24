7bc82554aad8f9a1413686276e1fd41c9ae199f7
Suvansu <suvansu@hotmail.com>
2026-01-24 11:25:08 -0500

    class activity 1

--- lecture.py
+++ lecture.py
@@ -1,2 +1,12 @@
 print("hello")
 print("trying new thing")
+
+name = "suvansu paudel"
+a = 10
+b = 20
+c = 30
+total = a + b + c 
+
+print("Hello my name is" , name , "my first variable is" , a, "my second variable is" , b, "my third variable is" , c, "when I add them together I get" , total)
+
+
